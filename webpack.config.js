827e169d712e2a92827e221cea6204b3b34f9e72
Marharyta Babkova <marharyta.babkova@meridianlink.com>
2024-12-08 14:59:04 +0100

    create react app with webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx", // Entry point of your app
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js", // Output file
+    clean: true, // Cleans up old builds
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Match .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/, // Match .css files
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // Resolve these extensions
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // HTML template
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000, // Development server port
+    open: true, // Open browser automatically
+  },
+  mode: "development", // Set the mode
+};
