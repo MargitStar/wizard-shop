6cf4e66f6173f471832604e98922f1caf26776d6
Marharyta Babkova <marharyta.babkova@meridianlink.com>
2024-12-15 20:29:30 +0100

    remove useless import

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
