8f38ce3f96d095e6f36889a4ee5dcf523613cee0
Marharyta Babkova <marharyta.babkova@meridianlink.com>
2025-02-23 15:43:47 +0100

    Add logout button

---
+++ src/components/LogoutButton/style.js
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+import styled from "@emotion/styled";
+
+export const StyledLogoutButton = styled(Button)`
+  background-color: #4b5945;
+  color: #fff;
+  &:hover {
+    background-color: #5d6d57;
+  }
+`;
