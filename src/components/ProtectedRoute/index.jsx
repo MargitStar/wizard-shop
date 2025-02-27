63b582a1c2b8e2f1dca3b37e9620a84dca61ffdb
Marharyta Babkova <marharyta.babkova@meridianlink.com>
2025-02-27 12:29:03 +0100

    Remove Fragment

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -2,13 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import React from "react";
 
 const ProtectedRoute = ({ isAuthorized }) => {
-  return isAuthorized ? (
-    <>
-      <Outlet />
-    </>
-  ) : (
-    <Navigate to="/login" replace />
-  );
+  return isAuthorized ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
