a2946f3b62e5f19af5ee10a174741ed3bf67a6a5
Marharyta Babkova <marharyta.babkova@meridianlink.com>
2025-02-27 12:45:40 +0100

    Fix Magic Card width

--- src/components/MagicCard/style.js
+++ src/components/MagicCard/style.js
@@ -2,11 +2,12 @@ import { Box, Card } from "@mui/material";
 import styled from "@emotion/styled";
 
 export const MagicCardBox = styled(Box)`
-  min-width: 275px;
-  max-width: 375px;
+  min-width: 300px;
+  max-width: 300px;
   height: 200px;
   flex: 1 1 calc(33.33% - 16px);
   margin-bottom: 2px;
+  white-space: wrap;
 `;
 
 export const StyledMagicCard = styled(Card)`
